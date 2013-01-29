82ff93fea97f31286f578c79e582604cfe7c05cc
Cody Tanner <cerebralstorn@gmail.com>
2013-01-28 19:49:18 -0700

    added makefile

--- Assignment4/tester.cc
+++ Assignment4/tester.cc
@@ -1,13 +1,38 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 #include "warehouse.h"
 #include "food_item.h"
 #include "date.h"
 
 using namespace inventory;
 
-int main ()
+int main(int argc, char *argv[])
 {
-  
+    // string file_name = argv[0];
+    // ifstream in(file_name);
+
+    // // Loop for reading the file.  Note that it is controlled
+    // //   from within the loop (see the 'break').
+    
+    // while (true)
+    // {
+    //   // Read a word (don't worry about punctuation)
+      
+    //   string word;
+    //   in >> word;
+
+    //   // If the read failed, we're probably at end of file
+    //   //   (or else the disk went bad).  Exit the loop.
+      
+    //   if (in.fail())
+    //     break;
+
+    //   cout << word << endl;
+    // }
+
+    // // Close the file.
+
+    // in.close();
   return 0;
 }
