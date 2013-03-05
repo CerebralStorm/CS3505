ed121510c0d0a25dbf710b7c91eafa7d885b0e09
Cody Tanner <cerebralstorn@gmail.com>
2013-03-05 14:41:43 -0700

    modified UTAH encoder

--- SpringProject1/ffmpeg/libavcodec/utahenc.c
+++ SpringProject1/ffmpeg/libavcodec/utahenc.c
@@ -32,10 +32,10 @@ AVCodec ff_utah_encoder = {
     .name           = "utah",
     .type           = AVMEDIA_TYPE_VIDEO,
     .id             = AV_CODEC_ID_UTAH,
-    .priv_data_size = 1024,
+    .priv_data_size = sizeof(UTAHEncContext),
     .init           = utah_enc_init,
     .encode2        = encode_frame,
     .capabilities   = NULL,
-    .pix_fmts       = NULL,
+    .pix_fmts       = AV_PIX_FMT_RGB24,
     .long_name      = NULL_IF_CONFIG_SMALL("UTAH (Built for CS 3505 in U of U) image"),
 };
