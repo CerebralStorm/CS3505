efc3f95cf319af43aba691b05b8e396aa3ceca47
Cody Tanner <cerebralstom@gmail.com>
2013-03-22 21:59:16 -0600

    blah

--- SpringProject1/bouncer/test.cc
+++ SpringProject1/bouncer/test.cc
@@ -50,19 +50,6 @@ int main(int argc, char *argv[]) {
   if(avformat_find_stream_info(pFormatCtx, NULL)<0)
     return -1; // Couldn't find stream information
   
-  // Dump information about file onto standard error
- // av_dump_format(pFormatCtx, 0, argv[1], 0);
-  
-  // Find the first video stream
-  /*videoStream=-1;
-  for(i=0; i<pFormatCtx->nb_streams; i++)
-    if(pFormatCtx->streams[i]->codec->codec_type==AVMEDIA_TYPE_VIDEO) {
-      videoStream=i;
-      break;
-    }
-  if(videoStream==-1)
-    return -1; // Didn't find a video stream
-*/
   // Get a pointer to the codec context for the video stream
   pCodecCtx=pFormatCtx->streams[0]->codec;
   
@@ -141,11 +128,18 @@ int main(int argc, char *argv[]) {
       pCodecCtx->codec = utahCodec;
       avcodec_encode_video2(pCodecCtx, &utahPacket, pFrameRGB, &gotPacket);
 
-      FILE *pFile;
-      pFile = fopen("frame.utah", "wb");
-      cout<<"Packet size = "<<utahPacket.size<<endl;
-      fwrite(utahPacket.data, 1, utahPacket.size, pFile);
-      fclose(pFile);
+      char filename[32];
+
+    
+      for(int i=1; i<=10; i++)
+      {
+          FILE *pFile;
+          snprintf(filename, sizeof(filename), "frame%03d.utah", i);
+          pFile = fopen(filename, "wb");
+          cout<<"Packet size = "<<utahPacket.size<<endl;
+          fwrite(utahPacket.data, 1, utahPacket.size, pFile);
+          fclose(pFile);
+      }
     }
     
     // Free the packet that was allocated by av_read_frame
